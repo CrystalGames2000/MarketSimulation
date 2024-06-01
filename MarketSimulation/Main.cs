167d0fa8fe09039471545bfd3d31699f15723737
CrystalGames2000 <ofirabe09@gmail.com>
2024-06-01 20:54:28 +0300

    Added 2 Scripts

---
+++ MarketSimulation/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarketSimulation.CrystalGames.MarketBase;
+
+namespace MarketSimulation
+{
+    internal class Main
+    {
+
+    }
+}
